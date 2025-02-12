e371b95895adc080bbb12e7a05ad678aa8ac4503
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-02-12 17:27:05 +0200

    Update streams.js

---
+++ static/js/streams.js
@@ -0,0 +1,4 @@
+const APP_ID = '741c8a7e9f944f3ca5b73ba3274128d7'
+const CHANNEL = 'main'
+const TOKEN = '007eJxTYEg6pbpu0eYd6Tf8D89YelvgwfXV5utac9knaB/bw7Pwg+RNBQZzE8Nki0TzVMs0SxOTNOPkRNMkc+OkRGMjoISRRYq5t9Pq9IZARoZZulMZGKEQxGdhyE3MzGNgAADiUyC5'
+let UID;
