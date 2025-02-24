252128147bf790e257b0b5ce3d7471ace6dbba0f
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-02-24 16:05:02 +0200

    Update streams.js

--- static/js/streams.js
+++ static/js/streams.js
@@ -1,8 +1,7 @@
-const APP_ID = '741c8a7e9f944f3ca5b73ba3274128d7'
+const APP_ID = ''
 const CHANNEL = 'main'
-const TOKEN = '007eJxTYEg6pbpu0eYd6Tf8D89YelvgwfXV5utac9knaB/bw7Pwg+RNBQZzE8Nki0TzVMs0SxOTNOPkRNMkc+OkRGMjoISRRYq5t9Pq9IZARoZZulMZGKEQxGdhyE3MzGNgAADiUyC5'
+const TOKEN = ''
 let UID;
-
 
 const client = AgoraRTC.createClient({mode: 'rtc', codec: 'vp8'})
 
@@ -10,10 +9,8 @@ let localTracks = []
 let remoteUsers = {}
 
 let joinAndDisplayLocalStream = async () => {
-    client.on('user-published', () => {
-        console.log('User has joined our room!')
-    })
-
+    client.on('user-published', handleUserJoined)
+    client.on('user-left', handleUserLeft)
 
     UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
 
@@ -23,8 +20,39 @@ let joinAndDisplayLocalStream = async () => {
                             <div class="username-wrapper"><span class="user-name">My Name</span></div>
                                <div class="video-player" id="user-${UID}"></div>
                         </div>`
-document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+    document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
     localTracks[1].play(`user-${UID}`)
     await client.publish([localTracks[0], localTracks[1]])
 
 }
+
+let handleUserJoined = async (user, mediaType) => {
+    remoteUsers[user.uid] = user
+    await client.subscribe(user, mediaType)
+
+    if (mediaType === 'video') {
+        let player = document.getElementById(`user-container-${user.uid}`)
+        if (player != null) {
+            player.remove()
+        }
+
+        player = `<div  class="video-container" id="user-container-${user.uid}">
+            <div class="video-player" id="user-${user.uid}"></div>
+            <div class="username-wrapper"><span class="user-name">${member.name}</span></div>
+        </div>`
+
+        document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+        user.videoTrack.play(`user-${user.uid}`)
+    }
+
+    if (mediaType === 'audio')
+        user.audioTrack.play()
+}
+
+
+let handleUserLeft = async (user) => {
+    delete remoteUsers[user.uid]
+    document.getElementById(`user-container-${user.uid}`).remove()
+}
+
+joinAndDisplayLocalStream()
