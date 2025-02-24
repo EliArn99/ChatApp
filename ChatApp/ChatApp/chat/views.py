d5a56522ccb5b7b5fbc01d3ca689c573880f5bd2
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-02-24 16:04:01 +0200

    Update views.py

--- ChatApp/ChatApp/chat/views.py
+++ ChatApp/ChatApp/chat/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import random
+import time
+from agora_token_builder import RtcTokenBuilder
+from .models import RoomMember
+import json
+from django.views.decorators.csrf import csrf_exempt
+
+
 
 # Create your views here.
+
+def lobby(request):
+    return render(request, 'base/lobby.html')
+
+def room(request):
+    return render(request, 'base/room.html')
+
